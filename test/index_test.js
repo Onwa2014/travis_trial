90282cbff82cb4c4479b008f3f10209469c13de7
Onwaba <onwab@projectcodex.co>
2017-01-13 14:29:42 +0200

    added files that are necessary for travis

---
+++ test/index_test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var greet = require('../index')
+
+    describe('greet function', function() {
+        it('should greet ', function () {
+            var result = greet("Amila");
+            assert.equal(result, 'Hello Amila!');
+        });
+    });
